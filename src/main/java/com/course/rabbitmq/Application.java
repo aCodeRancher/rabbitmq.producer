f52d8eb76ee39e7dfe80ae74244e2035fedcfcc2
Helen Ma <hhma@alumni.cpp.edu>
2023-12-13 21:30:44 -0500

    Add EmployeeJsonProducer

--- src/main/java/com/course/rabbitmq/Application.java
+++ src/main/java/com/course/rabbitmq/Application.java
@@ -1,6 +1,6 @@
 package com.course.rabbitmq;
 
-import com.course.rabbitmq.producer.HumanResourceProducer;
+import com.course.rabbitmq.producer.EmployeeJsonProducer;
 import com.course.rabbitmq.producer.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,14 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
-import java.util.concurrent.ThreadLocalRandom;
 
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
     @Autowired
-    private HumanResourceProducer humanResourceProducer;
+    private EmployeeJsonProducer employeeJsonProducer;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -25,7 +24,7 @@ public class Application implements CommandLineRunner {
    public void run (String... args) throws Exception{
         for (int i=0;i<5;i++){
              var emp = new Employee("emp-" + i ,  "Employee "+ i, LocalDate.now());
-             humanResourceProducer.sendMessage(emp);
+             employeeJsonProducer.sendMessage(emp);
         }
    }
 
