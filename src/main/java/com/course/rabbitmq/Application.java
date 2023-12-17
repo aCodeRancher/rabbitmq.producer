47845ffc56a816aad52688de89aaeaff3ab406c3
Helen Ma <hhma@alumni.cpp.edu>
2023-12-17 14:06:47 -0500

    add furniture producer

--- src/main/java/com/course/rabbitmq/Application.java
+++ src/main/java/com/course/rabbitmq/Application.java
@@ -1,32 +1,40 @@
 package com.course.rabbitmq;
 
-import com.course.rabbitmq.producer.PictureProducerTwo;
+import com.course.rabbitmq.producer.FurnitureProducer;
+import com.course.rabbitmq.producer.entity.Furniture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
     @Autowired
-    private PictureProducerTwo pictureProducer;
+    private FurnitureProducer furnitureProducer;
 
-    private final List<String> SOURCES = List.of("mobile", "web");
+    private final List<String> COLORS = List.of("white", "red", "green");
 
-    private final List<String> TYPES = List.of("jpg", "png", "svg");
+    private final List<String> MATERIALS = List.of("wood", "plastic", "steel");
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-   @Override
-   public void run (String... args) throws Exception{
-        
-   }
+    @Override
+     public void run (String... args) throws Exception{
+             for (int i=0;i<10;i++){
+                  var furniture = new Furniture();
+                  furniture.setName("Furniture " + i);
+                  furniture.setColor(COLORS.get(i % COLORS.size()));
+                  furniture.setMaterial(MATERIALS.get( i % MATERIALS.size()));
+                  furniture.setPrice(i);
+
+                 furnitureProducer.sendMessage(furniture);
+             }
+    }
 
 }
