d32521a64a2d17676bd157177d35a45e400c0bea
Helen Ma <hhma@alumni.cpp.edu>
2023-12-18 20:56:06 -0500

    retry fanout

--- src/main/java/com/course/rabbitmq/Application.java
+++ src/main/java/com/course/rabbitmq/Application.java
@@ -1,25 +1,21 @@
 package com.course.rabbitmq;
 
-import com.course.rabbitmq.producer.RetryPictureProducer;
-import com.course.rabbitmq.producer.entity.Picture;
+import com.course.rabbitmq.producer.RetryEmployeeJsonProducer;
+import com.course.rabbitmq.producer.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
     @Autowired
-    private RetryPictureProducer pictureProducer;
+    private RetryEmployeeJsonProducer producer;
 
-    private final List<String> SOURCES = List.of("mobile", "web");
 
-    private final List<String> TYPES = List.of("jpg", "png", "svg");
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -28,12 +24,9 @@ public class Application implements CommandLineRunner {
    @Override
    public void run (String... args) throws Exception{
         for (int i=0;i<10;i++){
-             var p  = new Picture();
-             p.setName("Picture " + i);
-             p.setSize(ThreadLocalRandom.current().nextLong(9001,10000));
-             p.setSource(SOURCES.get(i % SOURCES.size()));
-             p.setType(TYPES.get( i % TYPES.size()));
-             pictureProducer.sendMessage(p);
+             var emp  = new Employee("Employee-"+i, null, LocalDate.now());
+
+             producer.sendMessage(emp);
         }
    }
 
--- src/main/java/com/course/rabbitmq/producer/RetryEmployeeJsonProducer.java
+++ src/main/java/com/course/rabbitmq/producer/RetryEmployeeJsonProducer.java
@@ -3,23 +3,21 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.course.rabbitmq.producer.entity.Picture;
+import com.course.rabbitmq.producer.entity.Employee;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
-public class RetryPictureProducer {
-
+public class RetryEmployeeJsonProducer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     @Autowired
     private ObjectMapper objectMapper;
 
-    public void sendMessage(Picture p) throws JsonProcessingException {
-        var json = objectMapper.writeValueAsString(p);
+    public void sendMessage(Employee emp) throws JsonProcessingException {
+        var json = objectMapper.writeValueAsString(emp);
 
-        rabbitTemplate.convertAndSend("x.guideline.work", p.getType(), json);
+        rabbitTemplate.convertAndSend("x.guideline2.work", "", json);
     }
-
 }
