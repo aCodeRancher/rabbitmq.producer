75ba91952acb339094ad671a40f8eac17d448945
Helen Ma <hhma@alumni.cpp.edu>
2023-12-19 21:30:44 -0500

    add scheduler

--- src/main/java/com/course/rabbitmq/Application.java
+++ src/main/java/com/course/rabbitmq/Application.java
@@ -1,20 +1,18 @@
 package com.course.rabbitmq;
 
-import com.course.rabbitmq.producer.EmployeeJsonProducer;
-import com.course.rabbitmq.producer.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.LocalDate;
 
-
+@EnableScheduling
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
-    @Autowired
-    private EmployeeJsonProducer employeeJsonProducer;
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -22,10 +20,7 @@ public class Application implements CommandLineRunner {
 
    @Override
    public void run (String... args) throws Exception{
-        for (int i=0;i<5;i++){
-             var emp = new Employee("emp-" + i ,  "Employee "+ i, LocalDate.now());
-             employeeJsonProducer.sendMessage(emp);
-        }
+
    }
 
 }
